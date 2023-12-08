95418cd7c32c3da493386c6a36744e874f00d539
Chin-yu Hsu <chinyu.official@gmail.com>
2023-12-08 14:44:02 +0800

    remove files not in use

--- routes/movieReview/_middleware.dart
+++ routes/movieReview/_middleware.dart
@@ -5,12 +5,13 @@ import 'package:stormberry/stormberry.dart';
 Handler middleware(Handler handler) {
   return (context) async {
     final db = Database(
-        host: '127.0.0.1',
-        port: 5432,
-        database: 'postgres',
-        user: 'postgres',
-        password: '',
-        useSSL: false);
+      host: '127.0.0.1',
+      port: 5432,
+      database: 'postgres',
+      user: 'postgres',
+      password: '',
+      useSSL: false,
+    );
 
     final response = await handler
         .use(requestLogger())
